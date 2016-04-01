c5e81bfb3dd19b5f56b5a2e00d3b905a104bf0fb
newto_000 <newton205r5@gmail.com>
2016-04-01 20:02:44 +0200

    add missing files

---
+++ app/src/main/java/com/example/newto/korsarze/O_aplikacji.java
@@ -0,0 +1,13 @@
+package com.example.newto.korsarze;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class O_aplikacji extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_o_aplikacji);
+    }
+}
