b61084e17a203e8e600cbdbcb9e50ff98ca0d985
gkamil <kamil.gasik@student.wat.edu.pl>
2016-04-10 23:23:57 +0200

    Added files via upload

---
+++ app/src/main/java/com/example/newto/korsarze/ImageAdapter.java
@@ -0,0 +1,52 @@
+package com.example.newto.korsarze;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.example.newto.korsarze.R;
+
+
+public class ImageAdapter extends BaseAdapter {
+    public Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return 100;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+
+        if (convertView == null) {
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(100,100));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(3,3,3,3);
+        }
+        else
+        {
+            imageView = (ImageView) convertView;
+        }
+        imageView.setImageResource(mThumbIds[0]);
+        return imageView;
+    }
+
+    public Integer[] mThumbIds = {
+            R.drawable.water,R.drawable.ship,R.drawable.hit,R.drawable.unknown
+    };
+}
--- app/src/main/java/com/example/newto/korsarze/PrepareActivity.java
+++ app/src/main/java/com/example/newto/korsarze/PrepareActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class PrepareActivity extends AppCompatActivity {
 
@@ -11,7 +14,18 @@ public class PrepareActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prepare);
+
+        GridView gridView = (GridView) findViewById(R.id.prepareGrid);
+        gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id){
+                Toast.makeText(PrepareActivity.this,Integer.toString(position),Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
+
 
     public void OnClickPlay(View view)
     {
