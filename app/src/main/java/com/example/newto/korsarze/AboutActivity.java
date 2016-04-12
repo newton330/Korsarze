42fe4fbcffb598eede821ef3caa1ecc66b860830
michalciolek <michalciolek@gmail.com>
2016-04-12 14:26:49 +0200

    Super zmiana costam

--- app/src/main/java/com/example/newto/korsarze/AboutActivity.java
+++ app/src/main/java/com/example/newto/korsarze/AboutActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class AboutActivity extends AppCompatActivity {
+    //TODO
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
